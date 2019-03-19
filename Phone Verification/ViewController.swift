b7ae01bcc7bc9775b6ccc8bd372910f7d0c729aa
Brent Schooley <schooleybrent@gmail.com>
2019-03-19 14:50:39 -0700

    Initial Commit

---
+++ Phone Verification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phone Verification
+//
+//  Created by Brent Schooley on 3/19/19.
+//  Copyright © 2019 Brent Schooley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
