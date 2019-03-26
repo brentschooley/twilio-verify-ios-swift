6a5d65dc346a8c7580f78a1f43f269c1d98b9e82
Brent Schooley <schooleybrent@gmail.com>
2019-03-26 17:49:59 -0400

    Part 2 complete

---
+++ Phone Verification/CheckVerificationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CheckVerificationViewController.swift
+//  Phone Verification
+//
+//  Created by Brent Schooley on 3/26/19.
+//  Copyright © 2019 Brent Schooley. All rights reserved.
+//
+
+import UIKit
+
+class CheckVerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
